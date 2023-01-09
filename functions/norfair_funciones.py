2b7b7277f274166d9031fc126d28894fdd1a1854
Emanuel A. Cortez Medici <emanuelacortezm@gmail.com>
2023-01-09 16:26:47 -0300

    Orden

---
+++ functions/norfair_funciones.py
@@ -0,0 +1,119 @@
+import argparse
+from typing import List, Optional, Union
+import os
+import yolov5
+import numpy as np
+import torch
+import torchvision.ops.boxes as bops
+
+import norfair
+from norfair import Detection, Tracker, Video, Paths
+
+MAX_DISTANCE: int = 100000
+
+class YOLO:
+    def __init__(self, model_path: str, device: Optional[str] = None):
+        if device is not None and "cuda" in device and not torch.cuda.is_available():
+            raise Exception(
+                "Selected device='cuda', but cuda is not available to Pytorch."
+            )
+        # automatically set device if its None
+        elif device is None:
+            device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        # load model
+        self.model = yolov5.load(model_path, device=device)
+
+    def __call__(
+        self,
+        img: Union[str, np.ndarray],
+        conf_threshold: float = 0.25,
+        iou_threshold: float = 0.45,
+        image_size: int = 720,
+        classes: Optional[List[int]] = None
+    ) -> torch.tensor:
+
+        self.model.conf = conf_threshold
+        self.model.iou = iou_threshold
+        if classes is not None:
+            self.model.classes = classes
+        detections = self.model(img, size=image_size)
+        return detections
+
+
+def euclidean_distance(detection, tracked_object):
+    return np.linalg.norm(detection.points - tracked_object.estimate)
+
+
+def center(points):
+    return [np.mean(np.array(points), axis=0)]
+
+
+def iou_pytorch(detection, tracked_object):
+    # Slower but simplier version of iou
+
+    detection_points = np.concatenate([detection.points[0], detection.points[1]])
+    tracked_object_points = np.concatenate(
+        [tracked_object.estimate[0], tracked_object.estimate[1]]
+    )
+
+    box_a = torch.tensor([detection_points], dtype=torch.float)
+    box_b = torch.tensor([tracked_object_points], dtype=torch.float)
+    iou = bops.box_iou(box_a, box_b)
+
+    # Since 0 <= IoU <= 1, we define 1/IoU as a distance.
+    # Distance values will be in [1, inf)
+    return np.float(1 / iou if iou else MAX_DISTANCE)
+
+
+def iou(detection, tracked_object):
+    # Detection points will be box A
+    # Tracked objects point will be box B.
+
+    box_a = np.concatenate([detection.points[0], detection.points[1]])
+    box_b = np.concatenate([tracked_object.estimate[0], tracked_object.estimate[1]])
+
+    x_a = max(box_a[0], box_b[0])
+    y_a = max(box_a[1], box_b[1])
+    x_b = min(box_a[2], box_b[2])
+    y_b = min(box_a[3], box_b[3])
+
+    # Compute the area of intersection rectangle
+    inter_area = max(0, x_b - x_a + 1) * max(0, y_b - y_a + 1)
+
+    # Compute the area of both the prediction and tracker
+    # rectangles
+    box_a_area = (box_a[2] - box_a[0] + 1) * (box_a[3] - box_a[1] + 1)
+    box_b_area = (box_b[2] - box_b[0] + 1) * (box_b[3] - box_b[1] + 1)
+
+    # Compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + tracker
+    # areas - the interesection area
+    iou = inter_area / float(box_a_area + box_b_area - inter_area)
+
+    # Since 0 <= IoU <= 1, we define 1/IoU as a distance.
+    # Distance values will be in [1, inf)
+    return 1 / iou if iou else (MAX_DISTANCE)
+
+
+def yolo_detections_to_norfair_detections(
+    yolo_detections: torch.tensor,
+) -> List[Detection]:
+    """convert detections_as_xywh to norfair detections
+    """
+    norfair_detections: List[Detection] = []
+
+    detections_as_xyxy = yolo_detections.xyxy[0]
+    for detection_as_xyxy in detections_as_xyxy:
+        bbox = np.array(
+            [
+                [detection_as_xyxy[0].item(), detection_as_xyxy[1].item()],
+                [detection_as_xyxy[2].item(), detection_as_xyxy[3].item()]
+            ]
+        )
+        scores = np.array([detection_as_xyxy[4].item(), detection_as_xyxy[4].item()])
+        label = int(detection_as_xyxy[5].item())
+        norfair_detections.append(
+            Detection(points=bbox, scores=scores, label=label)
+        )
+
+    return norfair_detections
